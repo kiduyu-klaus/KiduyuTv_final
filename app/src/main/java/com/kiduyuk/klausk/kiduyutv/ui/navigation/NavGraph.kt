1e939ae2480171f73dc4f32862aaab3071cb923b
Klaus kiduyu - Kelvin Muiga King'ori <muigakiduyu1995@gmail.com>
2026-03-21 03:09:57 +0300

    Add SeasonEpisodes route to NavGraph and integrate SeasonEpisodesScreen with navigation support for TV show details.

--- app/src/main/java/com/kiduyuk/klausk/kiduyutv/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/kiduyuk/klausk/kiduyutv/ui/navigation/NavGraph.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.kiduyuk.klausk.kiduyutv.ui.screens.detail.MovieDetailScreen
+import com.kiduyuk.klausk.kiduyutv.ui.screens.detail.SeasonEpisodesScreen
 import com.kiduyuk.klausk.kiduyutv.ui.screens.detail.TvShowDetailScreen
 import com.kiduyuk.klausk.kiduyutv.ui.screens.home.HomeScreen
 import com.kiduyuk.klausk.kiduyutv.ui.screens.home.MoviesScreen
@@ -81,8 +82,34 @@ fun NavGraph(navController: NavHostController) {
                 onBackClick = { navController.popBackStack() },
                 onTvShowClick = { newTvId ->
                     navController.navigate(Screen.TvShowDetail.createRoute(newTvId))
+                },
+                onEpisodesClick = { id, name, totalSeasons ->
+                    navController.navigate(Screen.SeasonEpisodes.createRoute(id, name, totalSeasons))
                 }
+            )
+        }
+
+        composable(
+            route = Screen.SeasonEpisodes.route,
+            arguments = listOf(
+                navArgument("tvId") { type = NavType.IntType },
+                navArgument("totalSeasons") { type = NavType.IntType },
+                navArgument("tvShowName") {
+                    type = NavType.StringType
+                    defaultValue = ""
+                }
+            )
+        ) { backStackEntry ->
+            val tvId = backStackEntry.arguments?.getInt("tvId") ?: return@composable
+            val totalSeasons = backStackEntry.arguments?.getInt("totalSeasons") ?: 1
+            val tvShowName = backStackEntry.arguments?.getString("tvShowName") ?: ""
+            SeasonEpisodesScreen(
+                tvShowId = tvId,
+                tvShowName = tvShowName,
+                totalSeasons = totalSeasons,
+                onBackClick = { navController.popBackStack() }
             )
         }
     }
 }
+
